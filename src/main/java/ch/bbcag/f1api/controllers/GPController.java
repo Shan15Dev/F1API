2e54071d9169410ece3133a84acc12e809cc724c
zmuras <zmuras@bbcag.ch>
2022-01-18 13:14:24 +0100

    - GP is addable and removeable

--- src/main/java/ch/bbcag/f1api/controllers/GPController.java
+++ src/main/java/ch/bbcag/f1api/controllers/GPController.java
@@ -4,13 +4,21 @@ import ch.bbcag.f1api.models.GP;
 import ch.bbcag.f1api.repositories.GPRepository;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/gp")
@@ -21,17 +29,17 @@ public class GPController {
 
     @GetMapping()
     public Iterable<GP> findByParam(@RequestParam(required = false) String name, @RequestParam(required = false) Integer year) {
-        try{
-            if(Strings.isNotBlank(name) && year != null) {
+        try {
+            if (Strings.isNotBlank(name) && year != null) {
                 return gpRepository.findByYearAndLastName(name, year);
-            }else if (Strings.isNotBlank(name)) {
+            } else if (Strings.isNotBlank(name)) {
                 return gpRepository.findByWinnerLastName(name);
-            }else if (Strings.isBlank(name) && year != null){
+            } else if (Strings.isBlank(name) && year != null) {
                 return gpRepository.findByYear(year);
-            }else {
+            } else {
                 return gpRepository.findAll();
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -39,6 +47,25 @@ public class GPController {
     @GetMapping("{id}")
     public Iterable<GP> findById(@PathVariable int id) {
         return gpRepository.findById(id);
+    }
+
+
+    @PostMapping()
+    public void insert(@RequestBody @Valid GP gp){
+        gpRepository.save(gp);
+    }
+
+    @PutMapping
+    public void update(@RequestBody @Valid GP gp){
+        gpRepository.save(gp);
+    }
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Integer id){
+        try {
+            gpRepository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
     //@GetMapping()
